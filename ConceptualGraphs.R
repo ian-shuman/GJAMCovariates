0f7d58a4556142065f1ee9489ba0097a7f96af72
Ian Shuman <108153029+ian-shuman@users.noreply.github.com>
2024-08-26 11:57:52 -0400

    Adding taxon-level conceptual correlation matrices

--- ConceptualGraphs.R
+++ ConceptualGraphs.R
@@ -42,12 +42,23 @@ corrplot(mat2, diag = F, type = 'upper', method = c("circle"), tl.col = 'black',
 
 
 
+##Make conceptual corrplots for Taxon level 
+pal <- c('#364b9a', '#4a7bb7', '#6ea6cd', '#93cae1', '#cde4ef', 
+         '#eaeccc', '#feda8b', '#fdb336', '#a50026', '#dd3d2d', '#a50026') #Changed pal and circle size for aesthetics
+tax_data <- c(0.05, -0.5, -0.5, 0.05, 0.05, -0.5, 0.05, 0.05, -0.5, -0.5, -0.5, 0.05, 0.05, -0.5, -0.5, 0.05, -0.5, -0.5, 0.05, 0.05, 0.05, -0.5, -0.5, 0.05, 0.05)
+tax <- matrix(tax_data, nrow = 5, ncol = 5)
+colnames(tax) <- c("No tree", "Oak", "Hickory", "Beech", "Maple")
+rownames(tax) <- c("No tree", "Oak", "Hickory", "Beech", "Maple")
+corrplot(tax, diag = F, type = 'upper', method = c("circle"), tl.col = 'black', col = rev(pal), cl.cex = 1, tl.cex =2.5, cl.align.text = 'l', addgrid.col = NA)
+#Warning thrown bc of changes in pal
+
+tax_data2 <- c(0.05, -0.05, -0.05, 0.05, 0.05, -0.05, 0.05, 0.05, -0.05, -0.05, -0.05, 0.05, 0.05, -0.05, -0.05, 0.05, -0.05, -0.05, 0.05, 0.05, 0.05, -0.05, -0.05, 0.05, 0.05)
+tax2 <- matrix(tax_data2, nrow = 5, ncol = 5)
+colnames(tax2) <- c("No tree", "Oak", "Hickory", "Beech", "Maple")
+rownames(tax2) <- c("No tree", "Oak", "Hickory", "Beech", "Maple")
+corrplot(tax2, diag = F, type = 'upper', method = c("circle"), tl.col = 'black', col = rev(pal), cl.cex = 1, tl.cex =2.5, cl.align.text = 'l', addgrid.col = NA)
+#Warning thrown bc of changes in pal
 
 
 
 
-
-#x2 <- structure * rnorm(1, -.3, .2) + rnorm(1, .8, .2)
-structure2 <- structure * .9 + .15
-x3 <- x2 * .15 + .5
-plot(c(structure + .9, structure2), c(x2, x3))
